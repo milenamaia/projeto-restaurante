227ee478644de7df1c10ad71225a83fa64b03713
milenamaia <milenamaiadesouza123@gmail.com>
2022-10-18 15:58:35 -0300

    arrumando desgner no site

--- js/query.js
+++
@@ -1,9 +0,0 @@
-$('nav a').click(function(e){
-	e.preventDefault();
-	var id = $(this).attr('href'),
-			targetOffset = $(id).offset().top;
-			menuHeight = $('nav').innerHeight();
-	$('html, body').animate({ 
-		scrollTop: targetOffset - menuHeight
-	}, 500);
-});
